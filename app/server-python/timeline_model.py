c3f7decb8e0ecbac9a5b55ea82a37f149bbd1a7e
Thomas Kjeldahl Nilsson <thomanil@gmail.com>
2026-06-07 10:48:15 +0200

    fix error again

--- app/server-python/timeline_model.py
+++ app/server-python/timeline_model.py
@@ -7,7 +7,7 @@ def _linear(i: int) -> int:
     return i
 
 
-def _fibona  cci(i: int) -> int:
+def _fibonacci(i: int) -> int:
     """0, 1, 1, 2, 3, 5, 8, ..."""
     a, b = 0, 1
     for _ in range(i):
